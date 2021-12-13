1173fb3a07851f6b9229f9cfb99b0f8d0570c2dd
Paulopgfs <paulopgfs@gmail.com>
2021-12-13 15:19:08 -0300

    Criação de Classes

---
+++ desafioDioPoo/src/Main.java
@@ -0,0 +1,31 @@
+import br.com.diodesafio.dominio.Curso;
+import br.com.diodesafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java POO");
+        curso1.setDescricao("Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("JS");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+
+
+    }
+}
